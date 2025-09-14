d21f01e2d5fdc8859378af6f1b3fcf950ad61fe8
Emiliano Magana <websmithmxl@gmail.com>
2025-09-14 16:26:54 -0700

    Added state variables, styling, user input

--- LemonRistorante/AboutView.swift
+++ LemonRistorante/AboutView.swift
@@ -2,26 +2,52 @@
 //  AboutView.swift
 //  LemonRistorante
 //
-//  Created by Emiliano on 9/10/25.
-//
-
 import SwiftUI
 
 struct AboutView: View {
+    let userFirstName = "Michael"
+    let userLastName = "Myers"
+    @State var orderCount = 10
+    @State var clientName:String = ""
+    
     var body: some View {
         VStack {
-            Text("Welcome to Lemon Ristorante!")
+            Text("Hello \(userFirstName),")
                 .font(.title)
+                .fontWeight(.bold)
+                .lineLimit(1)
                 .padding()
-                
+            
+            Text("Welcome to Little Lemon")
+                .font(.body)
+            
             Image("littleLemonLogo")
                 .resizable()
                 .scaledToFit()
-                .frame(maxHeight:200)
+                .frame(width: 275, height: 170)
+            
+            Text("You have ordered \(orderCount) times.")
+                .padding(.top, 20)
+            
+            Button("Place a new order") {
+                orderCount += 1
+            }
+            .padding()
+            .buttonStyle(.borderedProminent)
+            
+            Button("Clear my orders"){
+                orderCount = 0
+            }
+            TextField("Enter your name: ", text:$clientName)
+                .padding()
+                .textFieldStyle(.roundedBorder)
+            Text ("Client name is working \(clientName)")
         }
+        
         .navigationTitle("About Us")
     }
 }
+
 #Preview {
     AboutView()
 }
--- Practice/Variables.playground/Contents.swift
+++ Practice/Variables.playground/Contents.swift
@@ -1,5 +1,37 @@
 import Cocoa
-
-var greeting = "Hello, playground"
+var greeting = "Yes"
 print(greeting)
-print("I have just fixed the structure of this project.")
+let day = "wednesday"
+print("today is \(day)")
+// Game score
+let levelScore = 10
+var gameScore = 0
+
+gameScore = gameScore + levelScore
+print("The gamescore is: \(gameScore) ")
+
+var levelBonusScore = 10.0
+levelBonusScore = 20
+print("The level bonus score is: \(levelBonusScore)")
+gameScore += Int(levelBonusScore)
+print("The game final score is \(gameScore)")
+
+/**
+ Create a swift program that declares three variables to store your
+ name, age, city.
+ Then use print() and string interpolation to display a sentence introducing yourself
+ */
+let myName = "Emiliano Magana"
+let myAge = 26.0
+let myCity = "Mexicali"
+print("Hello, my name is \(myName), I am \(myAge) years old and I am from \(myCity)")
+
+/**
+ Swift data types
+ */
+let dishName:String = "Pasta"
+var price:Double = 19.99
+var inStock:Bool = true
+let qty:Int = 3
+
+
