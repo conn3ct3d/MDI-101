62a20e5e4c80e528cc4fbaec7614e410c5574143
Emiliano Magana <websmithmxl@gmail.com>
2025-09-14 19:34:59 -0700

    added reservation form

--- LemonRistorante/ReservationForm.swift
+++ LemonRistorante/ReservationForm.swift
@@ -2,14 +2,26 @@
 //  ReservationForm.swift
 //  LemonRistorante
 //
-//  Created by mbair on 9/14/25.
+//  Created by Emiliano on 9/14/25.
 //
 
 import SwiftUI
 
 struct ReservationForm: View {
+    @State var userFirstName = ""
+    @State var guestCount = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("\(userFirstName) reservation for \(guestCount) people")
+        }
+        Form{
+            Section(header: Text("Reservation Details")){
+                TextField("Name: ", text: $userFirstName)
+                    
+                Stepper("Number of Guests: \(guestCount)", value: $guestCount, in: 1 ... 5)
+            }
+        }
+        
     }
 }
 
