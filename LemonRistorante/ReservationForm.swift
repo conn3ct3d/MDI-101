cd1e270aac7541d1131e25104a8463a843ea2c9a
Emiliano Magana <websmithmxl@gmail.com>
2025-09-14 20:31:27 -0700

    Added dynamic feedback

--- LemonRistorante/ReservationForm.swift
+++ LemonRistorante/ReservationForm.swift
@@ -10,21 +10,38 @@ import SwiftUI
 struct ReservationForm: View {
     @State var userFirstName = ""
     @State var guestCount = 0
-    var body: some View {
-        VStack{
-            Text("\(userFirstName) reservation for \(guestCount) people")
+    
+    // A computed property to generate the reservation status text
+    var reservationStatus: String {
+        if !userFirstName.isEmpty && guestCount > 0 {
+            return "\(userFirstName)'s reservation for \(guestCount) people"
+        } else if !userFirstName.isEmpty {
+            return "\(userFirstName)'s reservation"
+        } else if guestCount > 0 {
+            return "Reservation for \(guestCount) people"
+        } else {
+            return ""
         }
-        Form{
-            Section(header: Text("Reservation Details")){
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(reservationStatus)
+                .fontWeight(.bold)
+                .foregroundColor(.accentColor)
+                .padding(.horizontal)
+                .padding(.top, 20)
+        }
+        
+        Form {
+            Section(header: Text("Reservation Details")) {
                 TextField("Name: ", text: $userFirstName)
-                    
+                
                 Stepper("Number of Guests: \(guestCount)", value: $guestCount, in: 1 ... 5)
             }
         }
-        
     }
 }
-
 #Preview {
     ReservationForm()
 }
