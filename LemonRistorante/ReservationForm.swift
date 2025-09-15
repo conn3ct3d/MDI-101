1e1ac8ea171a22c9259f4c0eb951740c9f93dd26
Emiliano Magana <websmithmxl@gmail.com>
2025-09-14 18:14:52 -0700

    Added reservation form

---
+++ LemonRistorante/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  LemonRistorante
+//
+//  Created by mbair on 9/14/25.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
